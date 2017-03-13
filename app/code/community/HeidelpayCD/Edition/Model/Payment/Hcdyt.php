5270571a89503da18052c4592646c072bc5c4815
Jens Richter <development@heidelpay.de>
2017-03-13 17:37:15 +0100

    restore deprecated payment methods because of backward compatibly

---
+++ app/code/community/HeidelpayCD/Edition/Model/Payment/Hcdyt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Yapital payment method
+ *
+ * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present Heidelberger Payment GmbH. All rights reserved.
+ *
+ * @link  https://dev.heidelpay.de/magento
+ *
+ * @author  Jens Richter
+ *
+ * @package  Heidelpay
+ * @subpackage Magento
+ * @category Magento
+ *
+ * @deprecated This payment method is not longer available
+ */
+class HeidelpayCD_Edition_Model_Payment_Hcdyt extends HeidelpayCD_Edition_Model_Payment_Abstract
+{  
+	protected $_code = 'hcdyt';
+	protected $_canRefund = false;
+	protected $_canRefundInvoicePartial = false;
+	
+}
+
