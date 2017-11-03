cc01545cb863ac69ec51c175c58bc87347d27e1f
Stephano Vogel <stephano.vogel@outlook.com>
2017-11-03 10:20:07 +0100

    Added Santander Invoice payment method

---
+++ app/code/community/HeidelpayCD/Edition/Model/Payment/Hcdivsan.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class _Hcdivsan
+ */
+class HeidelpayCD_Edition_Model_Payment_Hcdivsan extends HeidelpayCD_Edition_Model_Payment_Abstract
+{
+    /** @var string */
+    protected $_code = 'hcdivsan';
+
+
+}
