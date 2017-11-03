8031048e25e2a6a4dd4f0cdf1e4c0c675d6d465b
Simon Gabriel <simon.gabriel@heidelpay.de>
2017-11-03 10:21:07 +0100

    [feature] Payolution: Added new payment type payolution (config, system, model)

---
+++ app/code/community/HeidelpayCD/Edition/Model/Payment/Hcdivpol.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Invoice unsecured payment method
+ *
+ * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present Heidelberger Payment GmbH. All rights reserved.
+ *
+ * @link  https://dev.heidelpay.de/magento
+ *
+ * @author  Simon Gabriel
+ *
+ * @package  Heidelpay
+ * @subpackage Magento
+ * @category Magento
+ */
+// @codingStandardsIgnoreLine magento marketplace namespace warning
+class HeidelpayCD_Edition_Model_Payment_Hcdivpol extends HeidelpayCD_Edition_Model_Payment_Abstract
+{
+    protected $_code = 'hcdivpol';
+
+//    /**
+//     * over write existing info block
+//     *
+//     * @var string
+//     */
+//    protected $_infoBlockType = 'hcd/info_invoice';
+//
+//    /**
+//     * Payment information for invoice mail
+//     *
+//     * @param array $paymentData transaction response
+//     *
+//     * @return string return payment information text
+//     */
+//    public function showPaymentInfo($paymentData)
+//    {
+//        /** @var HeidelpayCD_Edition_Model_Payment_Hcdivpol $loadSnippet */
+//        $loadSnippet = $this->_getHelper()->__('Invoice Info Text');
+//
+//        $repl = array(
+//            '{AMOUNT}' => $paymentData['CLEARING_AMOUNT'],
+//            '{CURRENCY}' => $paymentData['CLEARING_CURRENCY'],
+//            '{CONNECTOR_ACCOUNT_HOLDER}' => $paymentData['CONNECTOR_ACCOUNT_HOLDER'],
+//            '{CONNECTOR_ACCOUNT_IBAN}' => $paymentData['CONNECTOR_ACCOUNT_IBAN'],
+//            '{CONNECTOR_ACCOUNT_BIC}' => $paymentData['CONNECTOR_ACCOUNT_BIC'],
+//            '{IDENTIFICATION_SHORTID}' => $paymentData['IDENTIFICATION_SHORTID'],
+//        );
+//
+//        return strtr($loadSnippet, $repl);
+//    }
+//
+//    /**
+//     * @inheritdoc
+//     */
+//    public function processingTransaction($order, $data, $message='')
+//    {
+//        $message = Mage::helper('hcd')->__('received amount ')
+//            . $data['PRESENTATION_AMOUNT'] . ' ' . $data['PRESENTATION_CURRENCY'] . ' ' . $message;
+//        parent::processingTransaction($order, $data, $message);
+//    }
+}
